d0e19970f74c77b82fe09f951c6a68fc461bb949
Hong Yu Lin <linsanity@Hong-Yus-MacBook-Pro.local>
2025-03-30 20:52:03 -0400

    Added registration and login functionality

--- app.py
+++ app.py
@@ -1,16 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Enum
+from flask_bcrypt import Bcrypt
+from datetime import timedelta
 
 app = Flask(__name__)    
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///assignment3.db'
 app.config['SECRET_KEY'] = 'key'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 10)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def test():
-    return render_template('index.html')
+    return redirect('register.html')
+
+@app.route('/register.html', methods=['GET', 'POST'])
+def register():
+    if request.method=='GET':
+        return render_template('register.html')
+    else:
+        username = request.form["Username"]
+        email = request.form["Email"]
+        password = bcrypt.generate_password_hash(request.form['Password']).decode('utf-8')
+        accType = request.form["AccountType"]
+        registration = (username, email, password, accType)
+        print(registration)
+        addUser(registration)
+        return redirect("login.html")
+    
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+        if request.method=='GET':
+        #    if 'name' in session:
+        #            return redirect('index.html')
+            return render_template('login.html')
+        else:
+            username = request.form["Username"]
+            password = request.form["Password"]
+            user = Users.query.filter_by(username = username).first()
+            if not user or not bcrypt.check_password_hash(user.password, password):
+                flash('Please check your login details and try again.', 'Error')
+                return render_template('login.html')
+            
+            session['name'] = user.u_id
+            session.permanent = True
+            return redirect('index.html')
+
+
+def addUser(details):
+    user = Users(username=details[0], email=details[1], password=details[2], accType=details[3])
+    db.session.add(user)
+    db.session.commit()
 
 @app.route('/index.html')
 def index():
@@ -84,14 +126,14 @@ class Remark(db.Model):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        if not Users.query.first():
+        '''if not Users.query.first():
             person1 = Users(username='student1', email='student@test.com', password='pass', accType='Student')
             person2 = Users(username='teacher1', email='teacher@test.com', password='passer', accType='Teacher')
             db.session.add_all([person1, person2])
             db.session.commit()
 
         if not Grades.query.first():
-            grade1 = Grades(u_id=1, a_name="test1", percent=94.0)  # Ensure `percent` is a float
+            grade1 = Grades(u_id=1, a_name="test1", percent=94.0)
             db.session.add(grade1)
             db.session.commit()
 
@@ -103,6 +145,6 @@ if __name__ == '__main__':
         if not Remark.query.first():
             remark1 = Remark(u_id=1, status='rejected', a_name='final', reason='sick') 
             db.session.add(remark1)
-            db.session.commit()    
+            db.session.commit()'''
         db.session.close()
     app.run(debug=True)
